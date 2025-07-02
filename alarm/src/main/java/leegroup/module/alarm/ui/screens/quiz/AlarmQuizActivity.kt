a914544caeced9489a38ef623cc0b5514fa5a24a
Long Do <Longdt57@gmail.com>
2025-07-02 23:27:47 +0700

    Update Colors

--- alarm/src/main/java/leegroup/module/alarm/ui/screens/quiz/AlarmQuizActivity.kt
+++ alarm/src/main/java/leegroup/module/alarm/ui/screens/quiz/AlarmQuizActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -24,6 +25,7 @@ class AlarmQuizActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         showOverLockscreen()
+        enableEdgeToEdge()
         setContent {
             ComposeTheme {
                 val navController = rememberNavController()
